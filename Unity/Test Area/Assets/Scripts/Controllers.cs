42300d2cb4cc0d8c07511025c21f2482a16d40d2
dl960 <dl960@york.ac.uk>
2017-09-25 11:24:05 +0100

    VR Object movment basics
    +Added scripts for VR, picking up objects

---
+++ Unity/Test Area/Assets/Scripts/Controllers.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controllers : MonoBehaviour {
+    //Buttons
+    private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;  
+    private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;   
+
+    //Checking what order the controllers have been added to the game i
+    private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
+    private SteamVR_TrackedObject trackedObj;
+
+    HashSet<InteractibleObject> objectHoveringOver = new HashSet<InteractibleObject>();
+
+    private InteractibleObject closesObject;
+    private InteractibleObject interactingObject;
+
+    // Use this for initialization
+    void Start () {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }	
+	// Update is called once per frame
+	void Update () {
+		if(controller == null)
+        {
+            Debug.Log("Controller not initialized");
+            return;
+        }             
+        if (controller.GetPressDown(gripButton) )
+        {
+            float minDistance = float.MaxValue;
+            float distance;
+            foreach(InteractibleObject item in objectHoveringOver){
+                distance = (item.transform.position - transform.position).sqrMagnitude;
+                if(distance< minDistance)
+                {
+                    minDistance = distance;
+                    closesObject = item;
+                }
+            }
+            interactingObject = closesObject;
+            closesObject = null;
+            if (interactingObject){
+                if (interactingObject.IsInteracting())
+                {
+
+                    interactingObject.EndInteraction(this);
+                }
+                interactingObject.BeginInteraction(this);                           
+            }
+        }
+        if (controller.GetPressUp(gripButton) && interactingObject !=null )
+        {
+            interactingObject.EndInteraction(this);
+        }   
+    }
+    private void OnTriggerEnter(Collider collider)
+    {
+        InteractibleObject collidedObject = collider.GetComponent<InteractibleObject>();
+        if (collidedObject)
+        {
+            objectHoveringOver.Add(collidedObject;)
+        }
+    }
+    private void OnTriggerExit(Collider collider)
+    {
+        InteractibleObject collidedObject = collider.GetComponent<InteractibleObject>();
+        if (collidedObject)
+        {
+            objectHoveringOver.Remove(collidedObject);
+        }
+    }
+}
---
+++ Unity/Test Area/Assets/Scripts/InteractibleObject.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractibleObject : MonoBehaviour {
+
+    public Rigidbody rigidbody;
+    private bool currentlyInteracting;
+
+    //Stuff for object manipulation
+    private float velocityFactor = 20000f;
+    private Vector3 posDelta;
+    private float rotationFactor = 600f;
+    private Quaternion rotationDelta;
+    private float angle;
+    private Vector3 axis;
+
+    private Controllers attachedController;
+    private Transform interactionPoint;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+        interactionPoint = new GameObject().transform;
+        velocityFactor /= rigidbody.mass;
+        //try taking this out to make the rotations /w no delay
+        rotationFactor /= rigidbody.mass;
+	}	
+	// Do i need FIXED UPDATE for rigidbodu manipulation? (CHECK WHEN GAME GETS MORE FULL OF CONTENT)
+	void Update () {
+		if(attachedController && currentlyInteracting)
+        {
+            posDelta = attachedController.transform.position - interactionPoint.position;
+            this.rigidbody.velocity = posDelta * velocityFactor * Time.fixedDeltaTime;
+
+            rotationDelta = attachedController.transform.rotation * Quaternion.Inverse(interactionPoint.rotation);
+            rotationDelta.ToAngleAxis(out angle, out axis);
+
+            if(angle > 180){
+                angle -= 360;
+            }
+            this.rigidbody.angularVelocity = (Time.fixedDeltaTime * angle * axis);
+        }
+	}
+    public void BeginInteraction(Controllers controller)
+    {
+        attachedController = controller;
+        interactionPoint.position = controller.transform.position;
+        interactionPoint.rotation = controller.transform.rotation;
+        interactionPoint.SetParent(transform, true);
+
+        currentlyInteracting = true;
+    }
+    public void EndInteraction(Controllers controller)
+    {
+       if(controller == attachedController)
+        {
+            attachedController = null;
+            currentlyInteracting = false;
+        }
+    }
+    public bool IsInteracting()
+    {
+        return currentlyInteracting;
+    }
+}
